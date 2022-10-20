ade02a0750fbcf3cd39fd23524dd51fefa26ce3e
Aleksander <asava@sibmail.com>
2022-10-20 22:59:29 +0700

    slip

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import FAQs from '../../components/FAQs'
 import MainHeader from '../../components/MainHeader'
 import Programs from '../../components/Programs'
 import Values from '../../components/Values'
@@ -11,6 +12,7 @@ const Home = () => {
     <MainHeader/>
     <Programs/>
     <Values/>
+    <FAQs/>
     </>
   )
 }
