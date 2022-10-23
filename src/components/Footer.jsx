f7269be7db65c7dd08e07d634b5aaa030df50a3c
Aleksander <asava@sibmail.com>
2022-10-23 14:33:11 +0700

    dinner

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
   return (
     <footer>
         <div className="container footer__container">
-            <article>
+             <article>
                 <Link to='/' className='logo'>
                     <img src={Logo} alt="Footer Logo" />
                 </Link>
@@ -21,15 +21,38 @@ const Footer = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet optio, 
                 quia enim libero vitae ut sed quod doloremque accusamus ullam!
                 </p>
-                <div className="footer_social">
+                <div className="footer__social">
                     <a href="https://linkedin.com" target='_blank' rel="noreferrer noopener"><FaLinkedin/></a>
                     <a href="https://facebook.com" target='_blank' rel="noreferrer noopener"><FaFacebook/></a>
                     <a href="https://twitter.com" target='_blank' rel="noreferrer noopener"><AiOutlineTwitter/></a>
                     <a href="https://instagram.com" target='_blank' rel="noreferrer noopener"><AiFillInstagram/></a>
                 </div>
-            </article>
+             </article>
+             <article>
+                <h4>PermaLinks</h4>
+                <Link to="/about">About</Link>
+                <Link to="/plans">Plans</Link>
+                <Link to="/trainers">Trainers</Link>
+                <Link to="/gallery">Gallery</Link>
+                <Link to="/contact">Contact</Link>
+             </article>
+             <article>
+                <h4>Insights</h4>
+                <Link to="/s">Blog</Link>
+                <Link to="/s">Case Studies</Link>
+                <Link to="/s">Events</Link>
+                <Link to="/gallery">Communities</Link>
+                <Link to="/contact">FAQs</Link>
+             </article>
+             <article>
+                <h4>Get in Touch</h4>
+                <Link to="/contact">Contact Us</Link>
+                <Link to="/s">Support</Link>
+             </article>
         </div>
-        <article></article>
+        <div className="footer__copyright">
+            <small>2022 EGATOR TUTORIALS</small>
+        </div>
     </footer>
   )
 }
