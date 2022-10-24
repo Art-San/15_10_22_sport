bed38bdaf0b78dbdcf3d70b3e761f06dc347e984
Aleksander <asava@sibmail.com>
2022-10-24 18:01:34 +0700

    go

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = ({title, image, children}) => {
+  return (
+    <header className="header">
+        <div className="header__container">
+
+        </div>
+    </header>
+  ) 
+}
+
+export default Header
