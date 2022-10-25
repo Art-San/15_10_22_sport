9f67c7cd50b3f8f3b270eeb7251e8583a11fd799
Aleksander <asava@sibmail.com>
2022-10-25 23:47:10 +0700

    slip

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,8 @@
 import Header from '../../components/Header'
 import HeaderImage from '../../images/header_bg_1.jpg'
 import StoryImage from '../../images/about1.jpg'
+import VisionImage from '../../images/about2.jpg'
+import MissionImage from '../../images/about3.jpg'
 import './about.css'
 
 /* 3-28-00 закончил видео смотреть  */
@@ -18,7 +20,7 @@ const About = () => {
             <img src={StoryImage} alt="Our Story" />
           </div>
           <div className="about__section-content">
-            <h1>Our Storu</h1>
+            <h1>Our Story</h1>
             <p>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo eligendi animi 
               dolore consectetur totam necessitatibus maxime eum quisquam aut porro et repellat
@@ -38,13 +40,36 @@ const About = () => {
         </div>
       </section>
 
-      <section className="about__Vision">
-        <div className="container about__Vision-container">
-          <div className="about__section-image">
-            <img src={StoryImage} alt="Our Story" />
-          </div>
+      <section className="about__vision">
+        <div className="container about__vision-container">
           <div className="about__section-content">
             <h1>Our Vision</h1>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo eligendi animi 
+              dolore consectetur totam necessitatibus maxime eum quisquam aut porro et repellat
+              nam consequatur eaque deserunt dolores molestias officiis corrupti a officia dolorem, 
+              exercitationem facere numquam. Dignissimos, blanditiis distinctio? Nostrum!
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam itaque veritatis 
+              sint omnis, voluptatum maxime fugiat reprehenderit dolor quos optio mollitia. 
+              Error sit perferendis minus fugit non beatae voluptatum ad!
+            </p>
+          </div>
+          <div className="about__section-image">
+            <img src={VisionImage} alt="Our Vision" />
+          </div>
+        </div>
+      </section>
+
+
+      <section className="about__mission">
+        <div className="container about__mission-container">
+          <div className="about__section-image">
+            <img src={MissionImage} alt="Our Mission" />
+          </div>
+          <div className="about__section-content">
+            <h1>Our Mission</h1>
             <p>
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo eligendi animi 
               dolore consectetur totam necessitatibus maxime eum quisquam aut porro et repellat
