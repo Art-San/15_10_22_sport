a727bde4fbdfa494732928fd3251b30584949ebd
Aleksander <asava@sibmail.com>
2022-10-26 16:30:31 +0700

    go hospitl

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,27 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_2.jpg'
+import { MdEmail } from 'react-icons/md'
+import { BsMessenger } from 'react-icons/bs'
+import { IoLogoWhatsapp } from 'react-icons/io'
 import './contact.css'
+
+/* 3-40-00 надо заново смотреть  */
 
 const Contact = () => {
   return (
-    <div className='container'>Contact</div>
+    <>
+    <Header title='Get in Touch' image={HeaderImage}>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo consectetur officia maxime ipsum recusandae dolorum sunt, mollitia laborum.
+    </Header>
+    <section className="contact">
+      <div className="container contact__container">
+        <div className="contact__wrapper">
+          <a href="mailto:artsan.mcc@gmail.com" target="_blank" rel="noreferrer"><MdEmail/></a>
+        </div>
+      </div>
+    </section>
+    </>
+    
   )
 }
 
