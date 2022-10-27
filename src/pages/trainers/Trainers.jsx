7cd7390bfa0970477450abcd0fb8a35970df8b1c
Aleksander <asava@sibmail.com>
2022-10-27 19:38:06 +0700

    xavati

--- src/pages/trainers/Trainers.jsx
+++ src/pages/trainers/Trainers.jsx
@@ -4,12 +4,34 @@ import { trainers } from '../../data'
 import { BsInstagram } from 'react-icons/bs'
 import { AiOutlineTwitter } from 'react-icons/ai'
 import { FaFacebookF } from 'react-icons/fa'
-import { FaLinkedinIn } from 'react-icons'
+import { FaLinkedinIn } from 'react-icons/fa'
+import Trainer from '../../components/Trainer'
+
 import './trainers.css'
 
 const Trainers = () => {
   return (
-    <div className='container'>Trainers</div>
+    <>
+    <Header title='Our Trainers' image={HeaderImage} >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta et cum consequuntur ullam minus hic officiis atque?
+    </Header>
+    <div className="section trainers">
+      <div className="container trainers__container">
+        {
+          trainers.map(({id, image, name, job, socials}) => {
+            return <Trainer key={id} image={image} name={name} job={job} socials={
+              [
+                {icon: <BsInstagram/>, link: socials[0]},
+                {icon: <AiOutlineTwitter/>, link: socials[1]},
+                {icon: <FaFacebookF/>, link: socials[2]},
+                {icon: <FaLinkedinIn/>, link: socials[3]}
+              ]
+            }/>
+          })
+        }
+      </div>
+    </div>
+    </>
   )
 }
 
