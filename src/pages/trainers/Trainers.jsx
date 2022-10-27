62a708589b4e5aa8b0fe4370b2dd15829889b54c
Aleksander <asava@sibmail.com>
2022-10-27 17:49:13 +0700

    go12

--- src/pages/trainers/Trainers.jsx
+++ src/pages/trainers/Trainers.jsx
@@ -1,3 +1,10 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_5.jpg'
+import { trainers } from '../../data'
+import { BsInstagram } from 'react-icons/bs'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { FaFacebookF } from 'react-icons/fa'
+import { FaLinkedinIn } from 'react-icons'
 import './trainers.css'
 
 const Trainers = () => {
